f921db193cc7be1341561452ca17695258f74fd9
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2017-05-02 23:07:48 +0300

    initial commit

---
+++ src/main/java/com/github/toy/constructor/api/PerformStep.java
@@ -0,0 +1,17 @@
+package com.github.toy.constructor.api;
+
+import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
+
+import java.util.function.Consumer;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public interface PerformStep<THIS extends PerformStep<THIS>> {
+    default THIS perform(@Nullable String description, @NotNull Consumer<THIS> stepsConsumer) {
+        checkNotNull(description);
+        checkNotNull(stepsConsumer);
+        stepsConsumer.accept((THIS) this);
+        return (THIS) this;
+    }
+}
