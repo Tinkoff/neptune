97a1b88c5218c581ae0f5ed45862cca4905566a2
s.tikhomirov <stikhomirov@tinkoff.ru>
2017-12-18 14:13:25 +0300

    API improvements: raw function sequence

--- core.api/src/main/java/com/github/toy/constructor/core/api/DescriptionUtil.java
+++ core.api/src/main/java/com/github/toy/constructor/core/api/DescriptionUtil.java
@@ -4,6 +4,9 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+import static java.util.Objects.requireNonNull;
+import static java.util.Optional.ofNullable;
+
 public final class DescriptionUtil {
 
     public static <T> Consumer<T> action(String description, Consumer<T> consumer) {
@@ -15,6 +18,23 @@ public final class DescriptionUtil {
 
             public String toString() {
                 return description;
+            }
+
+            @Override
+            public Consumer<T> andThen(Consumer<? super T> after) {
+                Consumer<T> before = this;
+                requireNonNull(after);
+
+                return new Consumer<>() {
+                    @Override
+                    public void accept(T t) {
+                        before.accept(t); after.accept(t);
+                    }
+
+                    public String toString() {
+                        return before.toString() + " and then \n\n" + after.toString();
+                    }
+                };
             }
         };
     }
@@ -29,6 +49,41 @@ public final class DescriptionUtil {
             @Override
             public String toString() {
                 return description;
+            }
+
+            public <F> Function<F, R> compose(Function<? super F, ? extends T> before) {
+                requireNonNull(before);
+                Function<T, R> after = this;
+                return new Function<>() {
+                    @Override
+                    public R apply(F f) {
+                        T result = before.apply(f);
+                        return ofNullable(result).map(after).orElse(null);
+                    }
+
+                    @Override
+                    public String toString() {
+                        return before.toString() + " and then return " + after.toString();
+                    }
+                };
+            }
+
+            public <F> Function<T, F> andThen(Function<? super R, ? extends F> after) {
+                requireNonNull(after);
+                Function<T, R> before = this;
+
+                return new Function<>() {
+                    @Override
+                    public F apply(T t) {
+                        R result = before.apply(t);
+                        return ofNullable(result).map(after).orElse(null);
+                    }
+
+                    @Override
+                    public String toString() {
+                        return before.toString() + " and then return " + after.toString();
+                    }
+                };
             }
         };
     }
