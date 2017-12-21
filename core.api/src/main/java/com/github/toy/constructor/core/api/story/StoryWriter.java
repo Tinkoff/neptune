79551b5a50e287c4cf0184d29128a8c970e1f4cb
s.tikhomirov <stikhomirov@tinkoff.ru>
2017-12-21 14:26:43 +0300

    API improvements: java docs for the StoryWriter

--- core.api/src/main/java/com/github/toy/constructor/core/api/story/StoryWriter.java
+++ core.api/src/main/java/com/github/toy/constructor/core/api/story/StoryWriter.java
@@ -9,6 +9,16 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
 
 public final class StoryWriter {
 
+    /**
+     * This method creates a consumer with some string description. This consumer is
+     * supposed to perform some action.
+     *
+     * @param description string narration of the action
+     * @param consumer which performs the action
+     * @param <T> type of accepted value
+     * @return a new consumer with the given string description. Description is returned
+     * by the {@link #toString()} method.
+     */
     public static <T> Consumer<T> action(String description, Consumer<T> consumer) {
         checkArgument(!isBlank(description), "Description should not be empty");
         return new DescribedConsumer<>() {
@@ -24,6 +34,16 @@ public final class StoryWriter {
         };
     }
 
+    /**
+     * This method creates a function with some string description. This function is
+     * supposed to get some value.
+     *
+     * @param description string narration of the getting value
+     * @param function which gets the needed value
+     * @param <T> type of the input value
+     * @return a new function with the given string description. Description is returned
+     * by the {@link #toString()} method.
+     */
     public static <T, R> Function<T, R> toGet(String description, Function<T, R> function) {
         return new DescribedFunction<>() {
             @Override
@@ -38,6 +58,16 @@ public final class StoryWriter {
         };
     }
 
+    /**
+     * This method creates a predicate with some string description. This predicate is
+     * supposed to be used as some condition or filter.
+     *
+     * @param description string narration of the condition
+     * @param predicate which checks some input value
+     * @param <T> type of the input value
+     * @return a new predicate with the given string description. Description is returned
+     * by the {@link #toString()} method.
+     */
     public static <T> Predicate<T> condition(String description, Predicate<T> predicate) {
         return new DescribedPredicate<>() {
             @Override
