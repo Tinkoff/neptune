15ed35c2d56199048e692a2c6eddec0302ae5b26
TikhomirovSergey <tichomirovsergey@gmail.com>
2018-06-29 13:15:48 +0300

    Improved general build.gradle
    - fixed out dependency conflicts

---
+++ allure.testng.bridge/src/test/java/com/github/toy/constructor/allure/testng/bridge/test/AllureTestNGIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.github.toy.constructor.allure.testng.bridge.test;
+
+import com.github.toy.constructor.testng.integration.BaseTestNgTest;
+import org.testng.annotations.Test;
+
+public class AllureTestNGIntegrationTest extends BaseTestNgTest {
+
+    @Test
+    public void tezt() {
+
+    }
+}
