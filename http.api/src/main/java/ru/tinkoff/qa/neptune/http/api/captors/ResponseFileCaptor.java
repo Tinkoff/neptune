ceee6765089ac65c13b93f8644685d0e7aa685e7
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2018-12-16 20:23:04 +0300

    #56 Re-designed ResponseFileCaptor - Split ResponseFileCaptor to ResponseFileCaptor, ResponseJsonCaptor, and ResponseXmlCaptor

--- http.api/src/main/java/ru/tinkoff/qa/neptune/http/api/captors/ResponseFileCaptor.java
+++ http.api/src/main/java/ru/tinkoff/qa/neptune/http/api/captors/ResponseFileCaptor.java
@@ -15,20 +15,20 @@ import static java.lang.String.format;
 import static java.util.Optional.ofNullable;
 import static java.util.UUID.randomUUID;
 import static org.apache.commons.io.FileUtils.copyFile;
-import static org.apache.commons.io.FileUtils.writeStringToFile;
 
 /**
  * This class is designed to convert some {@link String} and {@link Path} bodies of received responses
  * to files.
  */
-public class ResponseFileCaptor extends FileCaptor<HttpResponse<?>> {
+public class ResponseFileCaptor extends FileCaptor<HttpResponse<Path>> {
 
     public ResponseFileCaptor() {
         super();
     }
 
     @Override
-    public HttpResponse<?> getCaptured(Object toBeCaptured) {
+    @SuppressWarnings("unchecked")
+    public HttpResponse<Path> getCaptured(Object toBeCaptured) {
         if (!HttpResponse.class.isAssignableFrom(toBeCaptured.getClass())) {
             return null;
         }
@@ -36,50 +36,29 @@ public class ResponseFileCaptor extends FileCaptor<HttpResponse<?>> {
         HttpResponse<?> response = (HttpResponse<?>) toBeCaptured;
         return ofNullable(response.body()).map(o -> {
             var clazz = o.getClass();
-            if (Path.class.isAssignableFrom(clazz) || String.class.isAssignableFrom(clazz)) {
-                return response;
+            if (Path.class.isAssignableFrom(clazz) && ((Path) o).toFile().exists()) {
+                return (HttpResponse<Path>) response;
             }
             return null;
         }).orElse(null);
     }
 
-    public void capture(HttpResponse caught, String message) {
+    public void capture(HttpResponse<Path> caught, String message) {
         super.capture(caught, format("Received response. %s", message));
     }
 
     @Override
-    protected File getData(HttpResponse caught) {
-        var body = caught.body();
+    protected File getData(HttpResponse<Path> caught) {
         var uuid = randomUUID().toString();
 
-        var clazz = body.getClass();
-        if (Path.class.isAssignableFrom(clazz)) {
-            var path = (Path) body;
-            File file = path.toFile();
+        File file = caught.body().toFile();
 
-            if (file.exists()) {
-                var absolutePath = file.getAbsolutePath();
-                try {
-                    var tempFile = createTempFile(format("%s_%s", getNameWithoutExtension(absolutePath), uuid),
-                            getFileExtension(absolutePath));
-                    tempFile.deleteOnExit();
-                    copyFile(file, tempFile);
-                    return tempFile;
-                } catch (IOException e) {
-                    return null;
-                }
-            }
-            else {
-                return null;
-            }
-        }
-
-        var stringBody = (String.valueOf(body));
+        var absolutePath = file.getAbsolutePath();
         try {
-            var tempFile = createTempFile(format("StringBodyOfResponse_%s", uuid),
-                    ".txt");
+            var tempFile = createTempFile(format("%s_%s", getNameWithoutExtension(absolutePath), uuid),
+                    getFileExtension(absolutePath));
             tempFile.deleteOnExit();
-            writeStringToFile(tempFile, stringBody, "UTF-8", true);
+            copyFile(file, tempFile);
             return tempFile;
         } catch (IOException e) {
             return null;
