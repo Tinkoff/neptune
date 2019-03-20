e3c78d2cff0763cb0582c86eea785af0031ea367
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2019-03-20 10:53:15 +0300

    TableCell implements Clickable

--- selenium/src/main/java/ru/tinkoff/qa/neptune/selenium/api/widget/drafts/TableCell.java
+++ selenium/src/main/java/ru/tinkoff/qa/neptune/selenium/api/widget/drafts/TableCell.java
@@ -1,12 +1,13 @@
 package ru.tinkoff.qa.neptune.selenium.api.widget.drafts;
 
+import ru.tinkoff.qa.neptune.selenium.api.widget.Clickable;
 import ru.tinkoff.qa.neptune.selenium.api.widget.HasValue;
 import ru.tinkoff.qa.neptune.selenium.api.widget.Name;
 import ru.tinkoff.qa.neptune.selenium.api.widget.Widget;
 import org.openqa.selenium.WebElement;
 
 @Name("Cell")
-public abstract class TableCell extends Widget implements HasValue<String> {
+public abstract class TableCell extends Widget implements HasValue<String>, Clickable {
     public TableCell(WebElement wrappedElement) {
         super(wrappedElement);
     }
