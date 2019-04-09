0aeb40a38238d44ae42e4eda58a1779be5ca12b4
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2019-04-09 20:53:38 +0300

    0.9.9-ALPHA Improvement of DBC

--- data.base.api/src/main/java/ru/tinkoff/qa/neptune/data/base/api/connection/data/DBConnectionSupplier.java
+++ data.base.api/src/main/java/ru/tinkoff/qa/neptune/data/base/api/connection/data/DBConnectionSupplier.java
@@ -23,18 +23,7 @@ public abstract class DBConnectionSupplier implements Supplier<DBConnection> {
     private static TransactionType DEFAULT_TRANSACTION_TYPE = RESOURCE_LOCAL;
     private static final Reflections REFLECTIONS = new Reflections("");
 
-    private final DBConnection connectionData;
-
-    public DBConnectionSupplier() {
-        var persistenceUnitMetaData = fillPersistenceUnit(new PersistenceUnitMetaData(this.getClass().getName(),
-                DEFAULT_TRANSACTION_TYPE.name(),
-                null));
-        REFLECTIONS.getSubTypesOf(PersistableObject.class).stream()
-                .filter(clazz -> nonNull(clazz.getAnnotation(PersistenceCapable.class)))
-                .map(Class::getName).collect(Collectors.toList()).forEach(persistenceUnitMetaData::addClassName);
-        connectionData = connectionData(persistenceUnitMetaData);
-    }
-
+    private DBConnection connectionData;
 
     /**
      * Fills given persistence unit with properties/parameters and returns filled object.
@@ -47,6 +36,15 @@ public abstract class DBConnectionSupplier implements Supplier<DBConnection> {
 
     @Override
     public DBConnection get() {
+        connectionData = ofNullable(connectionData).orElseGet(() -> {
+            var persistenceUnitMetaData = fillPersistenceUnit(new PersistenceUnitMetaData(this.getClass().getName(),
+                    DEFAULT_TRANSACTION_TYPE.name(),
+                    null));
+            REFLECTIONS.getSubTypesOf(PersistableObject.class).stream()
+                    .filter(clazz -> nonNull(clazz.getAnnotation(PersistenceCapable.class)))
+                    .map(Class::getName).collect(Collectors.toList()).forEach(persistenceUnitMetaData::addClassName);
+            return connectionData(persistenceUnitMetaData);
+        });
         return connectionData;
     }
 
