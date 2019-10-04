4b7cb3a194e1d7135616db5170c20ea8aaf45d77
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2019-10-04 21:39:31 +0300

    0.10.4-ALPHA

--- testng.integration/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.tinkoff.qa.neptune.testng.integration"},{"l":"ru.tinkoff.qa.neptune.testng.integration.properties"}]
