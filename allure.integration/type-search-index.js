7f2f06013be66b73e124496eea4f459a034fc809
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2019-10-10 19:45:13 +0300

    0.10.5-ALPHA

--- allure.integration/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.tinkoff.qa.neptune.allure","l":"AllureEventLogger"},{"p":"ru.tinkoff.qa.neptune.allure","l":"AllureFileInjector"},{"p":"ru.tinkoff.qa.neptune.allure","l":"AllureImageInjector"},{"p":"ru.tinkoff.qa.neptune.allure","l":"AllureStringInjector"}]
