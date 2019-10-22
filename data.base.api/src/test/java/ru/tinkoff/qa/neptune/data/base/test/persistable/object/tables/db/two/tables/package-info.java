eaade2d4452abae7afc486e94bc48f1238ccfcea
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2019-10-22 20:44:42 +0300

    Preparing for insert/delete/update testing

---
+++ data.base.api/src/test/java/ru/tinkoff/qa/neptune/data/base/test/persistable/object/tables/db/two/tables/package-info.java
@@ -0,0 +1,5 @@
+@ConnectionToUse(connectionSupplier = ConnectionDataSupplierForTestBase2.class)
+package ru.tinkoff.qa.neptune.data.base.test.persistable.object.tables.db.two.tables;
+
+import ru.tinkoff.qa.neptune.data.base.api.ConnectionToUse;
+import ru.tinkoff.qa.neptune.data.base.test.persistable.object.operations.ConnectionDataSupplierForTestBase2;
