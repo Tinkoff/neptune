1fcf65e3a737998470b98cfa919a0b652766dde1
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2019-10-28 09:52:55 +0300

    Re-design. Detach on transaction commit

--- data.base.api/src/main/java/ru/tinkoff/qa/neptune/data/base/api/DataBaseStepContext.java
+++ data.base.api/src/main/java/ru/tinkoff/qa/neptune/data/base/api/DataBaseStepContext.java
@@ -39,7 +39,6 @@ public class DataBaseStepContext implements GetStepContext<DataBaseStepContext>,
 
         return  ofNullable(manager).orElseGet(() -> {
             var newManager = (JDOPersistenceManager) connection.getConnectionFactory().getPersistenceManager();
-            newManager.getFetchPlan().setMaxFetchDepth(100000000);
             jdoPersistenceManagerSet.add(newManager);
             return newManager;
         });
