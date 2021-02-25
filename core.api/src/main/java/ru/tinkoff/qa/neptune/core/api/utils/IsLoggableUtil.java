4dff3142c27bd62df0533b3151005e48de9c6cca
s.tikhomirov <s.tikhomirov@tinkoff.ru>
2021-02-25 13:04:04 +0300

    IsLoggableUtil: improved

--- core.api/src/main/java/ru/tinkoff/qa/neptune/core/api/utils/IsLoggableUtil.java
+++ core.api/src/main/java/ru/tinkoff/qa/neptune/core/api/utils/IsLoggableUtil.java
@@ -1,7 +1,6 @@
 package ru.tinkoff.qa.neptune.core.api.utils;
 
 import static org.apache.commons.lang3.ClassUtils.isPrimitiveOrWrapper;
-import static org.apache.commons.lang3.StringUtils.containsIgnoreCase;
 
 public final class IsLoggableUtil {
 
@@ -20,21 +19,9 @@ public final class IsLoggableUtil {
             return true;
         }
 
-        var cls = clazz;
-        while (!cls.equals(Object.class)) {
-            try {
-                if (!containsIgnoreCase(clazz.getName(), "cglib")) {
-                    cls.getDeclaredMethod("toString");
-                    return true;
-                } else {
-                    cls = cls.getSuperclass();
-                }
-            } catch (NoSuchMethodException e) {
-                cls = cls.getSuperclass();
-            }
-        }
-
-        return false;
+        return !toBeDescribed.toString().equals(clazz.getName()
+                + '@'
+                + Integer.toHexString(toBeDescribed.hashCode()));
     }
 
 }
