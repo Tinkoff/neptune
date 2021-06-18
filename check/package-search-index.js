5a51bdc0799cafb6681cd1f22668a8253316b79b
s.tikhomirov <s.tikhomirov@tinkoff.ru>
2021-06-18 15:49:23 +0300

    0.18.0-ALPHA

--- check/package-search-index.js
+++ check/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.tinkoff.qa.neptune.check"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.tinkoff.qa.neptune.check"},{"l":"ru.tinkoff.qa.neptune.check.localization"}]
