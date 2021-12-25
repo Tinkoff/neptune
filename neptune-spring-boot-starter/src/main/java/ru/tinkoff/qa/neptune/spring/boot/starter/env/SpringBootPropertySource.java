03192d8d71fb3a09ef2a80c6666d96819c4950e4
s.tikhomirov <s.tikhomirov@tinkoff.ru>
2021-12-25 23:51:38 +0300

    0.20.20-ALPHA: - fixed bugs of auto-configs of Spring Boot for MockMvc and WebTestClient

--- neptune-spring-boot-starter/src/main/java/ru/tinkoff/qa/neptune/spring/boot/starter/env/SpringBootPropertySource.java
+++ neptune-spring-boot-starter/src/main/java/ru/tinkoff/qa/neptune/spring/boot/starter/env/SpringBootPropertySource.java
@@ -3,20 +3,22 @@ package ru.tinkoff.qa.neptune.spring.boot.starter.env;
 import ru.tinkoff.qa.neptune.core.api.properties.PropertySource;
 
 import static java.util.Optional.ofNullable;
-import static ru.tinkoff.qa.neptune.spring.boot.starter.env.SpringEnvironmentWrappingConfiguration.getCurrentApplicationEnvironment;
+import static ru.tinkoff.qa.neptune.spring.boot.starter.application.contexts.CurrentApplicationContextTestExecutionListener.getCurrentApplicationContext;
 
 public class SpringBootPropertySource implements PropertySource {
 
     @Override
     public String getProperty(String property) {
-        return ofNullable(getCurrentApplicationEnvironment())
+        var env = getCurrentApplicationContext().getEnvironment();
+        return ofNullable(env)
                 .map(environment -> environment.getProperty(property))
                 .orElse(null);
     }
 
     @Override
     public boolean isPropertyDefined(String property) {
-        return ofNullable(getCurrentApplicationEnvironment())
+        var env = getCurrentApplicationContext().getEnvironment();
+        return ofNullable(env)
                 .map(environment -> environment.containsProperty(property))
                 .orElse(false);
     }
