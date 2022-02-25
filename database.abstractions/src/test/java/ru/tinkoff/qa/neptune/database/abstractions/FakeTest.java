5c07d1dab9cc0a10c72874ea17d66ed818ff0db8
s.tikhomirov <s.tikhomirov@tinkoff.ru>
2022-02-25 21:15:29 +0300

    Test coverage metrics: - added jacoco plugin - improved gradle configs - updated dependencies

---
+++ database.abstractions/src/test/java/ru/tinkoff/qa/neptune/database/abstractions/FakeTest.java
@@ -0,0 +1,12 @@
+package ru.tinkoff.qa.neptune.database.abstractions;
+
+import org.testng.annotations.Test;
+
+
+public class FakeTest {
+
+    @Test
+    public void test() {
+        //todo needs for tests
+    }
+}
