68b59b8febdf9c90ff949cc07529ecf85fd713f8
s.tikhomirov <s.tikhomirov@tinkoff.ru>
2022-04-21 16:46:56 +0300

    Removal of deprecated integration with datanucleous

--- data.base.api/src/main/java/ru/tinkoff/qa/neptune/data/base/api/NothingIsSelectedException.java
+++
@@ -1,8 +0,0 @@
-package ru.tinkoff.qa.neptune.data.base.api;
-
-@Deprecated(forRemoval = true)
-public class NothingIsSelectedException extends RuntimeException {
-    public NothingIsSelectedException(String message) {
-        super(message);
-    }
-}
