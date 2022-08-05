2a6b227ead24c61689647af8dd93888172d6b771
Denis Prikhodko <39122755+Burnouttt@users.noreply.github.com>
2022-08-05 16:14:20 +0300

    fixed Allure attachments mime type

--- allure.integration/src/main/java/ru/tinkoff/qa/neptune/allure/AllureImageInjector.java
+++ allure.integration/src/main/java/ru/tinkoff/qa/neptune/allure/AllureImageInjector.java
@@ -23,11 +23,9 @@ public class AllureImageInjector implements CapturedImageInjector {
             picForLog = createTempFile("picture", randomUUID() + ".png");
             ImageIO.write(image, "png", picForLog);
             return new FileInputStream(picForLog);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new AllureResultsWriteException(e.getMessage(), e);
-        }
-        finally {
+        } finally {
             ofNullable(picForLog).ifPresent(file -> {
                 if (file.exists()) {
                     file.deleteOnExit();
