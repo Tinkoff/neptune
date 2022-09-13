d99dbbca18570d62255cbed4edd80017926975d5
a.charykov <a.charykov@tinkoff.ru>
2022-09-13 11:51:45 +0300

    add doc и тесты

--- rabbit.mq/src/main/java/ru/tinkoff/qa/neptune/rabbit/mq/function/get/RabbitMqBasicGetSupplier.java
+++ rabbit.mq/src/main/java/ru/tinkoff/qa/neptune/rabbit/mq/function/get/RabbitMqBasicGetSupplier.java
@@ -1,7 +1,7 @@
 package ru.tinkoff.qa.neptune.rabbit.mq.function.get;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import ru.tinkoff.qa.neptune.core.api.event.firing.annotations.MaxDepthOfReporting;
+import ru.tinkoff.qa.neptune.core.api.steps.annotations.MaxDepthOfReporting;
 import ru.tinkoff.qa.neptune.core.api.steps.SequentialGetStepSupplier;
 import ru.tinkoff.qa.neptune.core.api.steps.annotations.Description;
 import ru.tinkoff.qa.neptune.core.api.steps.annotations.DescriptionFragment;
